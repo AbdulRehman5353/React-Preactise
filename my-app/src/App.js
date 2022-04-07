1ba2c53668b0cb480cf9c63b51464be3e668fc75
AbdulRehman5353 <arehman535353@gmail.com>
2022-04-07 13:35:11 -0700

    ContextApi implemention

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,22 @@
+// import {useState, createContext } from "react";
 import './App.css';
-import FilterTags from './FilterTags';
+import Provider from "./Provider";
+import InputContent from "./input";
+import InputTitle from "./inputTitle";
+
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
 function App() {
+
   return (
     <div className="App">
-              <FilterTags/>
+           <Provider style={styles}>
+    <InputContent />
+    <InputTitle />
+  </Provider>
   </div>
   );
 }

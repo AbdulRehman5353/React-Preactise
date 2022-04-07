730dcbe6e26ecfa022efe7a66a8287e5761138d4
AbdulRehman5353 <arehman535353@gmail.com>
2022-04-07 07:50:55 -0700

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FilterTags from './FilterTags';
+
+function App() {
+  return (
+    <div className="App">
+              <FilterTags/>
+  </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/contextApi.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function contextApi(props) {
+  return (
+    <h1 style={{color:"white"}}>{props.test}</h1>
+  )
+}
